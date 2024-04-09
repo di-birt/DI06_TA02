f8354892cd3625e433d1336c807da52120cfdc1f
di-birt <mardanza@birt.eus>
2024-04-09 13:12:01 +0200

    calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { CalculadoraService } from '../services/calculadora.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+  mensaje: string = "";
+
+  constructor(private calculadoraService: CalculadoraService) {}
+
+  ngOnInit() {}
+
+  sumar(a: number, b: number): void {
+    this.resultado = this.calculadoraService.sumar(a, b);
+  }
+
+  restar(a: number, b: number): void {
+    this.resultado = this.calculadoraService.restar(a, b);
+  }
+
+  multiplicar(a: number, b: number): void {
+    this.resultado = this.calculadoraService.multiplicar(a, b);
+  }
+
+  dividir(a: number, b: number): void {
+    try {
+      this.resultado = this.calculadoraService.dividir(a, b);
+    } catch (error: any) {
+      //console.error(error.message);
+      this.resultado = NaN;
+      this.mensaje = error.message;
+    }
+  }
+
+  esNumeroValido(num: any): boolean {
+    return typeof num === 'number' && !isNaN(num);
+  }
 
 }
+
