73155ad7a64453e6e23c51ee059c43d3ef79d50e
di-birt <mardanza@birt.eus>
2024-04-11 12:21:46 +0200

    comentarios añadidos

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -28,7 +28,8 @@ describe('HomePage', () => {
     expect(component).toBeTruthy();
   });
 
-  //Comprobamos que los valores se inicializan con esos valores. Si intentamos poner por ejemplo toBe(10), nos dará error, ya que, los estamos inicializando con esos valores.
+  //Comprobamos que los valores se inicializan con esos valores. 
+  //Si intentamos poner por ejemplo toBe(10), nos dará error, ya que, los estamos inicializando con esos valores.
   it('debería inicializar correctamente las propiedades', () => {
     expect(component.numero1).toBe(0);
     expect(component.numero2).toBe(0);
@@ -36,34 +37,47 @@ describe('HomePage', () => {
     expect(component.mensaje).toBe('');
   });
 
+  //Espiamos sumarService y ejecutamos la lógica de sumar.
+  //Se espera que sumarService reciba los números 2 y 3 como parámetros
   it('debería llamar al método sumarService correctamente con 2 números desde el componente', () => {
     spyOn(calculadoraService, 'sumarService');
     component.sumar(2,3);
     expect(calculadoraService.sumarService).toHaveBeenCalledWith(2, 3);
   });
 
+  //Espiamos el método sumar, por tanto, no ejecutará la lógica de ese método.
+  //Se espera que sumar reciba los números 2 y 3 como parámetros
   it('debería llamar al método sumar correctamente con 2 números', () => {
     spyOn(component, 'sumar');
     component.sumar(2,3);
     expect(component.sumar).toHaveBeenCalledWith(2, 3);
   });
 
-  
+  //Ejecutamos directamente la lógica del método sumar.
+  //Si queremos ejecutar la lógica con un espía, necesitaremos añadirle '.and.callThrowugh()'
+  //Esperamos que la suma entre 2 y 3 devuelva 5
   it('debería sumar dos números correctamente', () => {
     //spyOn(component, 'sumar').and.callThrough();
     component.sumar(2, 3);
     expect(component.resultado).toBe(5);
   });
 
+  //Ejecutamos la lógica del método sumar mediante un espía.
+  //Para ello al espía le añadiremos '.and.callThrowugh()'
+  //Esperamos que la resta entre 5 y 3 devuelva 2
   it('debería restar dos números correctamente', () => {
     spyOn(component, 'restar').and.callThrough();
     component.restar(5, 3);
     expect(component.resultado).toBe(2);
   });
 
+  //Si queremos forzar que la simulación nos devuelva un valor que queramos.
+  //Podemos hacerlo mediante '.and.returnValue(valor)'
+  //En este caso estamos espiando el método multiplicarService, por tanto, el método multiplicar se ejecutará, pero multiplicarService no.
   it('debería multiplicar dos números correctamente', () => {
     spyOn(calculadoraService, 'multiplicarService').and.returnValue(6);
-    component.multiplicar(2, 3);
+    //Se ejecutará la lógica de este método, pero aunque hagamos 5*3, le hemos forzado que el valor devuelto por multiplicarService sea 6.
+    component.multiplicar(5, 3);
     expect(component.resultado).toBe(6);
   });
 
@@ -73,10 +87,14 @@ describe('HomePage', () => {
     expect(component.resultado).toBe(2);
   });
 
+  //En dividirService controlamos que si el segundo valor es 0, el espía nos lance una excepción.
+  //Para ello haremos uso de ".and.throwError('texto del error')"
   it('debería manejar correctamente la excepción al dividir por cero', () => {
     spyOn(calculadoraService, 'dividirService').and.throwError('No se puede dividir por cero');
     component.dividir(6, 0);
+    //Esperamos que el resultado sea un número
     expect(component.resultado).toBeNaN();
+    //Esperamos que el mensaje recibido sea la siguiente
     expect(component.mensaje).toBe('No se puede dividir por cero');
   });
 
--- src/app/services/calculadora.service.spec.ts
+++ src/app/services/calculadora.service.spec.ts
@@ -14,18 +14,25 @@ describe('CalculadoraService', () => {
     expect(service).toBeTruthy();
   });
 
+  //Mediante un espía controlamos el método sumarService
+  //Como estamos espiando el método sumarService, no se ejecutará su lógica
+  //Se espera que el método sumarService reciba los parámetros númericos 2 y 3
   it('debería llamar correctamente al método sumarService', () => {
     spyOn(service, 'sumarService');
     service.sumarService(2,3);
     expect(service.sumarService).toHaveBeenCalledWith(2,3);
   });
 
+  //Mediante un espía y callThrough ejecutamos la lógica del método sumarService
+  //El resultado esperado será 5
   it('debería sumar dos números correctamente', () => {
     spyOn(service, 'sumarService').and.callThrough();
     const resultado = service.sumarService(2, 3);
     expect(resultado).toBe(5);
   });
 
+  //Ejecuta la lógica del método restarService sin espias.
+  //El resultado esperado será 2
   it('debería restar dos números correctamente', () => {
     const resultado = service.restarService(5, 3);
     expect(resultado).toBe(2);
@@ -41,6 +48,9 @@ describe('CalculadoraService', () => {
     expect(resultado).toBe(2);
   });
 
+  //El método toThrowError de jasmine espera una función como argumento, por ello tendremos que añadir ()=> antes de la llamada al método.
+  //Ejecutará la lógica del método dividirService y si el segundo número es 0, lanzará un Error que la recogeremos aquí.
+  //Esperamos que el error sea el texto -> 'No se puede dividir por cero'
   it('debería lanzar un error al intentar dividir por cero', () => {
     expect(() => service.dividirService(6, 0)).toThrowError('No se puede dividir por cero');
   });
